d0285a051bcaabeb75a96218bb410c7347fbae92
Bia <beatriz.m.m.magalhaes@gmail.com>
2018-11-13 09:48:18 -0200

    Fixed zPosition bug in cards

--- speakAut/CardViewModel.swift
+++ speakAut/CardViewModel.swift
@@ -18,20 +18,17 @@ class CardViewModel: SKSpriteNode {
     private var wordNode = SKLabelNode()
     private var imageNode = SKSpriteNode()
     private var cardModel: Card!
-    
-    //Criar um Array com os índices pré estabelecidos na classe Card a partir do cardModel provido. Esse array será verificado pelo .is
-    private var cardIndex = Int()
-    
+
     init(cardModel:Card) {
         self.cardModel = cardModel
         let word = cardModel.word
         let image = cardModel.imageName
-        super.init(texture: nil, color: .clear, size: CGSize.card)
+        super.init(texture: nil, color: .blue, size: CGSize.card)
         createCard()
+        self.zPosition = 2
         self.wordNode.text = word
         imageNode.texture = SKTexture(imageNamed: image)
         self.isUserInteractionEnabled = true
-        self.zPosition = 7
         self.name = "cardModel"
         self.addChild(card)
     }
@@ -42,7 +39,7 @@ class CardViewModel: SKSpriteNode {
     
     func imageSetUp() {
         self.imageNode = SKSpriteNode(color: .red, size: CGSize.cardImage)
-        imageNode.zPosition = 5
+       // imageNode.zPosition = 2
         imageNode.position = CGPoint(x: 0, y: 20)
         imageNode.name = "image"
         card.addChild(imageNode)
@@ -53,14 +50,14 @@ class CardViewModel: SKSpriteNode {
         wordNode.fontColor = UIColor.greyishBrown
         wordNode.fontSize = 32
         wordNode.position = CGPoint(x: 0, y: -125)
-        wordNode.zPosition = 5
+       // wordNode.zPosition = 2
         wordNode.name = "word"
         card.addChild(wordNode)
     }
     
     func cardSetUp() {
         self.card = SKSpriteNode(texture: SKTexture(imageNamed: "blankCard"))
-        card.zPosition = 4
+        card.zPosition = 3
         card.name = "blankCard"
     }
     
@@ -75,7 +72,19 @@ class CardViewModel: SKSpriteNode {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        print("touchesBegan")
+        guard let brothers = self.parent?.allDescendants() else {return}
+        let cards = brothers.filter {($0.name?.starts(with: "card") ?? false)}
+        
+        let cardBros = cards.filter { $0 != self }
+        
+        print("cardbros ", cardBros)
+        
+        for index in cardBros {
+//            index.zPosition = 6
+            self.zPosition = 3
+            index.zPosition = self.zPosition - 1
+        }
+        print("my name is ", self.name, self.zPosition)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -95,20 +104,24 @@ class CardViewModel: SKSpriteNode {
         let cards = brothers.filter {($0.name?.starts(with: "card") ?? false)}
         let gaps = brothers.filter {($0.name?.starts(with: "gap") ?? false)}
         let cardArray = cards as! [CardViewModel]
-
         
-        print(cardArray.isOrderedInX)
         
+        //Check if cards are in the correct order when all gaps are filled
         if cards.near(gaps) {
             if cardArray.isOrderedInX {
                 print("cards are correct")
             } else {
                 print("cards are incorrect")
-                
             }
         }
-
-
+        
+        if cards.near(gaps) {
+            if cards.near(cards){
+                self.run(SKAction.move(to: CGPoint(x: 300, y: 200), duration: 0.3))
+            }
+        }
+        
+        //Have cards stick to gaps
         if let index = self.near(gaps) {
             let stickAnimation = SKAction.move(to: CGPoint(x: gaps[index].position.x, y:gaps[index].position.y ), duration: 0.2)
             stickAnimation.timingMode = .easeOut
