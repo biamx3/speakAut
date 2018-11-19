c2c3c0b856d5e383e2b9c42870dc698157c70631
Bia <beatriz.m.m.magalhaes@gmail.com>
2018-11-19 14:13:10 -0200

    Error message and invisible node to stop user interaction

--- speakAut/ViewModel/CardSetViewModel.swift
+++ speakAut/ViewModel/CardSetViewModel.swift
@@ -17,12 +17,13 @@ class CardSetViewModel: SKSpriteNode {
     
     private var cards: [CardViewModel] = []
     private var gaps: [GapViewModel] = []
+    private var invisibleNode: SKSpriteNode!
     
     init(cardSet: [Card]){
         super.init(texture: nil, color: .clear, size: UIScreen.main.bounds.size)
         self.name = "setOfCardsAndGaps"
         self.isUserInteractionEnabled = true
-
+        setUpInvisibleNode()
         addCards(from: cardSet)
         addGaps(from: cards)
     }
@@ -60,7 +61,68 @@ class CardSetViewModel: SKSpriteNode {
             let gap = GapViewModel()
             gap.position = CGPoint(x: card.position.x, y:  (sceneSize?.height ?? 0.0) - 180)
             self.addChild(gap)
+        }
+    }
+    
+    func errorMessage() -> SKLabelNode {
+        let sceneSize = self.frame.size
+        let errorMessage = SKLabelNode(text: "Tente novamente")
+        errorMessage.name = "instructionsLabel"
+        errorMessage.fontSize = 32
+        errorMessage.fontColor = UIColor.greyishBrown
+        errorMessage.zPosition = 15
+        errorMessage.name = "error message"
+        errorMessage.position = CGPoint(x: 0, y: sceneSize.height/6.7)
+        errorMessage.alpha = 0
+        
+        return errorMessage
+    }
+    
+    func setUpInvisibleNode() {
+        let sceneSize = self.frame.size
+        invisibleNode = SKSpriteNode(color: .clear, size: sceneSize)
+        invisibleNode.isUserInteractionEnabled = false
+        invisibleNode.zPosition = 20
+    }
+    
+    func cardsAreWrong(cardNodes: [CardViewModel]){
+        let errorLabel = errorMessage()
+        self.addChild(errorLabel)
+        
+        //Turn off user interaction
+        self.addChild(invisibleNode)
+        self.isUserInteractionEnabled = false
+        
+        let fadeIn = SKAction.fadeAlpha(to: 3.0, duration: 0.8)
+        let wait = SKAction.wait(forDuration: 2.0)
+        let fadeOut = SKAction.fadeOut(withDuration: 0.5)
+        let animation = SKAction.sequence([fadeIn, wait,fadeOut])
 
+        errorLabel.run(animation, completion: {
+            errorLabel.removeFromParent()
+            self.removeAllCardsFromGaps(cards: cardNodes)
+            })
+    }
+    
+    func randomRotation() -> SKAction {
+        let randomAngle = Float.random(in: -0.5 ..< 0.5)
+        let randomRotation = SKAction.rotate(byAngle: CGFloat(randomAngle), duration: 0.15)
+        return randomRotation
+    }
+    
+    func removeAllCardsFromGaps(cards: [CardViewModel]){
+        //Animate cards moving out of gap
+        let removeCardFromGap = SKAction.move(by: CGVector(dx: 0, dy: CGSize.card.height*1.1), duration: 0.2)
+        let randomRotation = self.randomRotation()
+        let removeCardFromGapGroup = SKAction.group([removeCardFromGap, randomRotation])
+        removeCardFromGapGroup.timingMode = .easeOut
+        
+        for card in cards {
+            card.run(removeCardFromGapGroup, completion: {
+                //Turn on user interaction
+                self.invisibleNode.removeFromParent()
+                self.isUserInteractionEnabled = true
+            })
         }
     }
     
@@ -78,6 +140,7 @@ class CardSetViewModel: SKSpriteNode {
             if cardViews.isOrderedInX {
                 print("cards are correct")
             } else {
+                cardsAreWrong(cardNodes: cardViews)
                 print("cards are incorrect")
             }
         }
